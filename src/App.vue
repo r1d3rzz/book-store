ce47790b7610dc847156ec5aa587acee0c141378
r1d3rzz <myintthwaykhine127@gmail.com>
2023-05-04 15:39:36 +0630

    create livechat ui

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,36 @@
     <div class="row">
       <NavBar />
     </div>
+    <div v-if="user">
+      <div class="liveChat" v-if="isChat">
+        <ChatForm @closeChatRoomModel="isChat = false" :user="user" />
+      </div>
+      <div class="chatBtnContainer" @click="isChat = !isChat">
+        <div>
+          <i class="far fa-comment-alt fs-4"></i>
+        </div>
+        <div>Live Chat</div>
+      </div>
+    </div>
   </div>
   <router-view />
 </template>
 
 <script>
+import { ref } from "vue";
+import ChatForm from "./components/chat/ChatForm";
 import NavBar from "./components/NavBar.vue";
+import getUser from "./composables/getUser";
 export default {
   components: {
+    ChatForm,
     NavBar,
+  },
+  setup() {
+    let { user } = getUser();
+    let isChat = ref(false);
+
+    return { isChat, user };
   },
 };
 </script>
@@ -22,5 +43,17 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+}
+.chatBtnContainer {
+  background-color: white;
+  width: 100px;
+  height: auto;
+  position: fixed;
+  right: 10px;
+  bottom: 20px;
+  padding: 5px;
+  border-radius: 5px;
+  user-select: none;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/chat/ChatForm.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="backdrop">
+    <div class="card chatDisplayContainer text-start">
+      <div
+        class="card-header d-flex justify-content-between align-items-center"
+      >
+        <div>
+          <div>
+            Hello <span class="fw-bold">{{ user.displayName }}</span>
+          </div>
+          <div class="text-muted">logged as {{ user.email }}</div>
+        </div>
+        <button class="btn btn-sm btn-primary" @click="closeChatRoom">
+          Close
+        </button>
+      </div>
+      <div class="card-body">
+        <div class="chatTextContainer">
+          <div v-for="text in 20" :key="text">
+            <div class="mb-3">
+              <span class="chatText">this is testing</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="input-group">
+            <input
+              type="text"
+              placeholder="Type here..."
+              class="form-control group-item"
+            />
+            <button class="btn btn-sm btn-primary">
+              <i class="fas fa-paper-plane fa-lg" style="padding: 0 5px"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  emits: ["closeChatRoomModel"],
+  props: ["user"],
+  setup(props, { emit }) {
+    let user = props.user;
+
+    let closeChatRoom = () => {
+      emit("closeChatRoomModel");
+    };
+
+    return { closeChatRoom, user };
+  },
+};
+</script>
+<style scoped>
+.chatDisplayContainer {
+  background-color: white;
+  width: 400px;
+  height: auto;
+  position: fixed;
+  right: 100px;
+  bottom: 85px;
+  border-radius: 5px;
+}
+.backdrop {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.568);
+}
+.chatText {
+  background-color: lightgrey;
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 15px;
+  border-bottom-left-radius: 0;
+}
+
+.chatTextContainer {
+  height: 400px;
+  overflow: auto;
+}
+</style>
