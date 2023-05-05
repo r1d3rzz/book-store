d77fe4a417c10837def593f5d01d534e090e6d98
r1d3rzz <myintthwaykhine127@gmail.com>
2023-05-05 13:27:47 +0630

    divided message form logged user view

--- src/components/chat/ChatDisplay.vue
+++ src/components/chat/ChatDisplay.vue
@@ -1,23 +1,18 @@
 <template>
   <div class="chatTextContainer" ref="messageDisplay">
     <div v-for="message in formattMessages" :key="message.id">
-      <div class="mb-3">
-        <span class="fw-bold me-2">{{ message.name }}</span>
-        <small class="text-muted">{{ message.created_at }}</small>
-        <br />
-        <span class="chatText">
-          <span>{{ message.message }}</span>
-        </span>
-      </div>
+      <SingleChatText :message="message" />
     </div>
   </div>
 </template>
 <script>
+import SingleChatText from "./SingleChatText";
 import { db } from "@/firebase/config";
 import { formatDistanceToNow } from "date-fns";
 import { computed, onUpdated, ref } from "vue";
 
 export default {
+  components: { SingleChatText },
   setup() {
     let messages = ref([]);
     let messageDisplay = ref(null);
@@ -49,14 +44,6 @@ export default {
 };
 </script>
 <style scoped>
-.chatText {
-  background-color: rgba(211, 211, 211, 0.601);
-  padding: 5px 10px;
-  border-radius: 5px;
-  font-size: 15px;
-  border-bottom-left-radius: 0;
-}
-
 .chatTextContainer {
   height: 330px;
   overflow: auto;
---
+++ src/components/chat/SingleChatText.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="mb-3"
+    :class="{
+      'text-end': user.displayName === message.name,
+    }"
+  >
+    <span class="fw-bold me-2">{{ message.name }}</span>
+    <small class="text-muted">{{ message.created_at }}</small>
+    <br />
+    <span
+      class="chatText"
+      :class="{ 'text-border': user.displayName === message.name }"
+    >
+      <span>{{ message.message }}</span>
+    </span>
+  </div>
+</template>
+<script>
+import getUser from "@/composables/getUser";
+
+export default {
+  props: ["message"],
+  setup() {
+    let { user } = getUser();
+
+    return { user };
+  },
+};
+</script>
+<style>
+.chatText {
+  background-color: rgba(211, 211, 211, 0.792);
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 15px;
+  border-bottom-left-radius: 0;
+}
+.text-border {
+  background-color: rgba(173, 216, 230, 0.673);
+  border-bottom-left-radius: 5px;
+  border-top-right-radius: 0;
+}
+</style>
